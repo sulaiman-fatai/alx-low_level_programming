cf0fb0007b08ff32c3d7e4993b04a87b58264ddf
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-12 21:12:24 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *
  *main - Entry point
+ *
  *Decription: a C program that prints the size of various types on *the computer it is compiled and run on.
- *Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 
 		printf("Size of a char: %zu byte(s)\n", sizeof(char));
