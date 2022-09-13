6817357d2acae404b38a1d602124ea51681c4820
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-12 20:37:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  *puts: string
+ *
+ *   *Return: Always 0 (Success)
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+  	        return (0);
+
+}
