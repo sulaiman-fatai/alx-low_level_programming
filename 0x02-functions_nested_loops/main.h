56f197ff30e3d4edab758b14588a17803c8d458f
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-13 20:38:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * _putchar: It writes a c character to stdout
+ * Return: On success 1.
+ * On error, -1 is returned, and error is not appropriate.
+ */
+
+int _putchar(char c);
+
+/**
+ * print_alphabet - print lowercase alphabet
+ */
+
+void print_alphabet(void);
