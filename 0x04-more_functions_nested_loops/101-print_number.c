6ff29b45e1bfe33331b7e09f34665b83c7a8af14
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-15 10:04:38 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,45 @@
  */
 void print_number(int n)
 {
-        _putchar('\n', n );
-	return (0);
+	int d = 1, i = 0, ii = 0;
+
+	if (n < 0)
+
+	{
+
+		_putchar('-');
+
+		n = -n;
+
+	}
+
+	while (n / d != 0)
+
+	{
+
+		d *= 10;
+
+		i++;
+
+	}
+
+	d = d / 10;
+
+	while (ii < i)
+
+	{
+
+		_putchar('0' + n / d);
+
+		n = n - (n / d) * d;
+
+		d = d / 10;
+
+		ii++;
+
+	}
+
+	if (i == 0)
+
+		_putchar('0' + n);
 }
