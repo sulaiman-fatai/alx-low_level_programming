69112719345387546cc57b469769cf1398509adf
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-15 10:17:35 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <math.h>
 /**
- * main - prints prime factors of 612852475143
- * Return: Always 0 (Success)
+ * main - prime numbers
+ * Return: Always 0.
  */
 int main(void)
-{
 
-	return (0);
+{
+unsigned int long n = 612852475143, a = (int) sqrt(n);
+while (1)
+
+{
+	if (n % a == 0)
+
+	{
+
+		printf("%lu \n", n / a);
+
+		break;
+
+	}
+
+	a--;
+
+}
+
+return (0);
 
 }
