b8c94dc05780a63f290a06069a59241712fef4c4
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-16 23:01:18 -0700

    4-puts.c error debug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *puts: string
@@ -7,10 +6,8 @@
  *Return: Always 0 (Success)
  *
  **/
-
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
-  	        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
