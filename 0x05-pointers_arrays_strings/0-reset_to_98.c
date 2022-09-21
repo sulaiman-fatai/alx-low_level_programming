14a354931bcda88354fdef292a75cdbdf029aab4
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-20 21:53:33 -0700

    array

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be updated
+ *Return: et_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be update
+ void reset_to_98(int *n)
+
+ {
+ *n = 98;
+ }
