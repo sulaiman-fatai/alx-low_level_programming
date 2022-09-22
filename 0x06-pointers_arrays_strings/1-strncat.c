cec168f56c844dffe852e274e4b6941c6cb48150
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-22 10:04:23 -0700

    edit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @src: The string to be appended
  * Return: A pointer to the resulting string dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int *n)
 {
 	int index = 0, dest_len = 0;
 
