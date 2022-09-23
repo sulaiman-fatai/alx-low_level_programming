c2cfeb329d304b268551ceb1bc610bb4a3cadcfd
Sulaiman Fatai <gfsfatai@yahoo.com>
2022-09-22 20:50:30 -0700

    betty check 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -13,6 +13,7 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+
 	while (*(n1 + c1) != '\0')
 		c1++;
 	while (*(n2 + c2) != '\0')
@@ -20,9 +21,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (c1 >= c2)
 		bg = c1;
 	else
-        	bg = c2;
+		bg = c2;
 	if (size_r <= bg + 1)
-		return(0);
+		return (0);
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
@@ -32,7 +33,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (op >= 10)
 			add = op / 10;
 		else
-	        	add = 0;
+			add = 0;
 		if (op > 0)
 		*(r + bg) = (op % 10) + 48;
 		else
